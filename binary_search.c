e5471a754cfcbd949c91a8fa9ddb5524fd8135e1
Himanshu singh <166948839+HimanshuS19@users.noreply.github.com>
2024-12-05 19:10:57 +0530

    Add files via upload

---
+++ binary_search.c
@@ -0,0 +1,44 @@
+//The Below Program is for Binary Search Implementation in c using VS CODE
+//------3rd sem AIML-1----------BY HIMANSHU SINGH (054)---------------//
+#include<Stdio.h>
+#include <stdlib.h>
+ int main ()
+{
+ int i,n,item,loc,beg,end,mid;                                     
+ printf("\nEnter the total number of elements: \t");
+ scanf("%d",&n);
+ 
+ int arr[n];
+
+for(i=0;i<n;i++)
+ {printf("\t Enter elements %d :", i+1);
+ scanf("%d",&arr[i]);
+ }
+printf("\n Array Elements : ");
+{
+ for(i=0;i<n;i++)
+  printf("\t %d", arr[i]);
+}
+printf("\nenter the element to be search by Binary Search\t");
+scanf("%d",&item);
+beg=0;
+end=n-1; 
+
+while(beg<=end)
+{ mid=(beg+end)/2;
+    
+    if(arr[mid]==item)
+    {
+        loc=mid;
+        printf("\n item %d is at %d location \t",arr[mid],loc+1);
+         exit(0);
+    }
+if (item<arr[mid])
+end=mid-1;
+if(item>arr[mid])
+beg=mid+1;
+mid=(beg+end)/2;
+}
+if(item>arr[n-1])
+printf("\n item is not in the list");
+}
